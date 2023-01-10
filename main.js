9401834379ffe939be3fd8b27350e6effbde2461
michaeljohnson8971 <michaeljohnson8971@gmail.com>
2023-01-10 13:06:00 -0500

    updated CSS and added javascript, no bonus

---
+++ main.js
@@ -0,0 +1,12 @@
+ 
+
+ // Define a Function
+ function sayOuch() {
+ 	alert('Ouch! Stop poking me!');
+ }
+ 
+ // Use a CSS selector to identify an element
+ var foxImage = document.querySelector('img');
+ 
+ // Assign the function to the onclick event on that element
+ foxImage.onclick = sayOuch;
